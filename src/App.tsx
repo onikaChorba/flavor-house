3f9ce5591a3ce07aec71b28bc2d89cbbf859ff99
Onika Chorba <110539259+onikaChorba@users.noreply.github.com>
2026-05-14 16:24:37 +0300

    about us

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import { Header } from './components';
-import { Home, Menu, Promo } from './pages';
+import { Home, Menu, Promo, About } from './pages';
 
 function App() {
 
@@ -13,6 +13,7 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/menu' element={<Menu />} />
           <Route path='/promo' element={<Promo />} />
+          <Route path='/about' element={<About />} />
         </Routes>
       </main>
     </Router>
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,6 @@
 import { Home } from "./Home";
 import { Menu } from "./Menu";
 import { Promo } from "./Promo";
+import { About } from "./About"
 
-export { Home, Menu, Promo };
+export { Home, Menu, Promo, About };
