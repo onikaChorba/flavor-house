dabc50e718691dbbf6e0e9eb5ab3dfdb7f5f7d13
Onika Chorba <110539259+onikaChorba@users.noreply.github.com>
2026-05-14 14:37:50 +0300

    page promo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import { Header } from './components';
-import { Home, Menu } from './pages';
+import { Home, Menu, Promo } from './pages';
 
 function App() {
 
@@ -12,6 +12,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/menu' element={<Menu />} />
+          <Route path='/promo' element={<Promo />} />
         </Routes>
       </main>
     </Router>
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,5 @@
 import { Home } from "./Home";
 import { Menu } from "./Menu";
+import { Promo } from "./Promo";
 
-export { Home, Menu };
+export { Home, Menu, Promo };
